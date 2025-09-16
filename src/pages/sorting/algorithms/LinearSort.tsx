cdeca1c929b00b8c168235502957d3aca9b13e36
Richard Jang <jang.richard7017@gmail.com>
2025-09-16 00:03:10 -0500

    init vite project and laid down basic routing foundation

---
+++ src/pages/sorting/algorithms/LinearSort.tsx
@@ -0,0 +1,5 @@
+export default function LinearSort() {
+    return <div>
+        Linear Sort
+    </div>
+}
---
+++ src/pages/sorting/algorithms/Route.tsx
@@ -0,0 +1,11 @@
+import type { RouteObject } from "react-router-dom";
+
+import LinearSort from "@pages/sorting/algorithms/LinearSort";
+import BubbleSort from "@pages/sorting/algorithms/BubbleSort";
+
+const Route: RouteObject[] = [
+    { path: "linear", element: <LinearSort /> },
+    { path: "bubble", element: <BubbleSort /> },
+]
+
+export default Route;
